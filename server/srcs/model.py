13ed77e75efb8671464eb6c1ec7179704959c7e0
sindydwns <sindydwns@naver.com>
2024-09-01 21:55:57 +0900

    asdf

--- server/srcs/model.py
+++ server/srcs/model.py
@@ -1,30 +1,28 @@
+import numpy as np
 import torch
 from tqdm import tqdm
-
-# input [*]
-# output [x, y, r]
 
 class FurnitureModel(torch.nn.Module):
     def __init__(self):
         super(FurnitureModel, self).__init__()
-        self.add_model = torch.nn.Sequential(
-            torch.nn.Linear(1, 128),
+        self.loc_model = torch.nn.Sequential(
+            torch.nn.Linear(16, 128),
             torch.nn.ReLU(),
             torch.nn.Linear(128, 64),
         )
         self.env_model = torch.nn.Sequential(
-            torch.nn.Linear(1, 128),
+            torch.nn.Linear(72, 256),
             torch.nn.ReLU(),
-            torch.nn.Linear(128, 64),
+            torch.nn.Linear(256, 64),
         )
         self.fusion_model = torch.nn.Sequential(
             torch.nn.Linear(128, 128),
             torch.nn.ReLU(),
-            torch.nn.Linear(128, 1)
+            torch.nn.Linear(128, 3)
         )
     
     def forward(self, add, env):
-        add_feat = self.add_model(add)
+        add_feat = self.loc_model(add)
         env_feat = self.env_model(env)
         concat_feat = torch.cat([add_feat, env_feat])
         out = self.fusion_model(concat_feat)
@@ -36,9 +34,9 @@ optim = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = torch.nn.MSELoss()
 epochs = 200
 
-X_train1 = torch.FloatTensor([0])
-X_train2 = torch.FloatTensor([0])
-y_train = torch.FloatTensor([1])
+X_train1 = torch.FloatTensor(np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
+X_train2 = torch.FloatTensor(np.array([0,0,0,0,6,6,0,1,1,0,0,6,0,0,1,2,0,0,0,6,0,1,3,6,0,0,6,0,1,4,0,6,6,0,0,2,5,2,0,2,0,0,0,0,0,0,0,0]))
+y_train = torch.FloatTensor(np.array([0, 0, 0]))
 
 model.train(True)
 X_train1 = X_train1.to(device)
@@ -51,4 +49,4 @@ for epoch in range(epochs):
     loss.backward()
     optim.step()
 
-    print(f"loss {pred.item()}\t{loss.item()}")
+    print(f"loss {pred}\t{loss.item()}")
